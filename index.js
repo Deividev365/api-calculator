48733082decb9935ec1cfa589a44d87d7bc66357
Deivid Almeida <deividalmeida365@gmail.com>
2021-11-07 16:07:39 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//Framework hapi.js
+const Hapi = require('hapi');
+
+// logic port and machine
+const host = 'localhost';
+const port = 8000; 
+
+// Create Server
+const server = Hapi.Server({
+    host: host,
+    port: port
+});
+
+// Initiate Server
+const init = async () => {
+
+    await server.start();
+    console.log("Servidor Ligado na porta " + port);
+
+}
+
+
+// Define routes // 
+
+require('./routes/routes')(server)
+
+
+
+
+//Initiate app
+init();
+
+
